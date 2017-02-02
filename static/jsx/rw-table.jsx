6ac2eaebea72c6598df4024409c21e7255c95cb2
p6vital <p6vital@gmail.com>
2017-02-02 19:00:21 +0000

    Temp code

--- static/jsx/rw-table.jsx
+++ static/jsx/rw-table.jsx
@@ -50,17 +50,94 @@ class RWTable extends React.Component {
         });
     }
 
+    getRateDisplayDoms() {
+        var regionMap = {};
+        var oddityMap = {};
+        var colorMap = {};
+        for (var i = 0; i < this.state.rwTableData.length; i++) {
+            var rwTableRow = this.state.rwTableData[i];
+
+            if (!regionMap[rwTableRow.region]) {
+                regionMap[rwTableRow.region] = 1;
+            }
+            else {
+                regionMap[rwTableRow.region]++;
+            }
+
+            if (!oddityMap[rwTableRow.oddity.toUpperCase()]) {
+                oddityMap[rwTableRow.oddity.toUpperCase()] = 1;
+            }
+            else {
+                oddityMap[rwTableRow.oddity.toUpperCase()]++;
+            }
+
+            if (!colorMap[rwTableRow.color.toUpperCase()]) {
+                colorMap[rwTableRow.color.toUpperCase()] = 1;
+            }
+            else {
+                colorMap[rwTableRow.color.toUpperCase()]++;
+            }
+        }
+
+        var regionTotal = 0;
+        for (var region in regionMap) {
+            regionTotal += regionMap[region];
+        }
+        var regionRateDoms = [];
+        for (var region in regionMap) {
+            regionRateDoms.push(<span key={region}>{region}: {(regionMap[region]/regionTotal).toFixed(2)}&nbsp;</span>);
+        }
+
+        var oddityTotal = 0;
+        for (var oddity in oddityMap) {
+            oddityTotal += oddityMap[oddity];
+        }
+        var oddityRateDoms = [];
+        for (var oddity in oddityMap) {
+            oddityRateDoms.push(<span key={oddity}>{oddity}: {(oddityMap[oddity]/oddityTotal).toFixed(2)}&nbsp;</span>);
+        }
+
+        var colorTotal = 0;
+        for (var color in colorMap) {
+            colorTotal += colorMap[color];
+        }
+        var colorRateDoms = [];
+        for (var color in colorMap) {
+            colorRateDoms.push(<span key={color}>{color}: {(colorMap[color]/colorTotal).toFixed(2)}&nbsp;</span>);
+        }
+
+        return (
+            <div>
+                <div>
+                    R:&nbsp;
+                    {regionRateDoms}
+                </div>
+                <div>
+                    O:&nbsp;
+                    {oddityRateDoms}
+                </div>
+                <div>
+                    C:&nbsp;
+                    {colorRateDoms}
+                </div>
+            </div>
+        );
+    }
+
     render() {
         var rowDoms = [];
         for (var i = 0; i < this.state.rwTableData.length; i++) {
             rowDoms.push(
                 <RWTableRow 
+                    key={i}
                     region={this.state.rwTableData[i].region}
                     oddity={this.state.rwTableData[i].oddity}
                     color={this.state.rwTableData[i].color}
                 />
             );
         }
+
+        var rateDom = this.getRateDisplayDoms();
 
         return (
             <div className="rw-table">
@@ -94,6 +171,7 @@ class RWTable extends React.Component {
                             onClick={this.addNew.bind(this)}
                     />
                 </div>
+                {rateDom}
             </div>
         );
     }
